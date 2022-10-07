97fd52df613f44f24f6983aacfce60b14d39ae7f
Adam Aske <adamaske@live.no>
2022-10-07 03:29:33 +0200

    byttet til pygame

--- graphics/graphics.py
+++ graphics/graphics.py
@@ -1,44 +1,29 @@
 import sys
-from PyQt5.QtCore import *
-from PyQt5.QtGui import * 
-from PyQt5.QtWidgets import *
-
+import pygame
+import os
 class Graphics():
     def __init__(self):
-        print("Setup Graphics!")
-        #Create window
-        app = QApplication(sys.argv)
-        self.win = Window()
-        self.win.show()
-        self.ShowImage('/hammer.png')
-        app.exec_()
-    def ShowImage(self, name):
-        print("Show image!")
-        self.win.showImage(name)
+        #Set up window
+        self.screen = pygame.display.set_mode((c.HEIGHT, c.WIDTH))
+        self.screen.fill((255,255,255))
 
+        #Load image into image
+        self.image = pygame.image.load('Images/hammer.png')
+    def ShowImage(self, path):
+       print("Show Image!")
+       self.image = pygame.image.load(os.path.join('Images', path))
+    def UpdateGraphics(self):
+        self.screen.blit(self.image, (0,0))
+        pygame.display.update()
+        
 
-class Window(QMainWindow):
-    def __init__(self):
-        super(Window, self).__init__()
-        self.setGeometry(200,200,300,300)
-        self.setWindowTitle("Python")
-        self.initUI()
-
-    def initUI(self):
-        self.label = QLabel(self)  
-
-    def showImage(self, path):
-        pixmap = QPixmap(path)
-        print(path)
-        self.label.setPixmap(pixmap)
-        self.resize(pixmap.width(),pixmap.height())  
-        self.show()  
 
 if __name__ == '__main__':
     #When run from this file
     import constants as c
     import helpers as h
     g = Graphics()
+    g.ShowImage("", 50,50)
 else:
     import graphics.constants as c
     import graphics.helpers as h
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 #Triggering the entire project
+from sys import exit
 #Do this by python run.py
 #Imports c as an object
 import constants as c
@@ -6,10 +7,24 @@ import constants as c
 import helpers as h
 #Graphics
 from graphics.graphics import Graphics
+#Pygame
+import pygame
+pygame.init()
 
 def run():
     g = Graphics()
-    g.ShowImage("/Images/hammer.png")
+    i = 0
+    g.ShowImage("hammer.png")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+        g.UpdateGraphics()
+        
+
+
+    
 
 #Convention for checking if this is the first file ran
 if __name__ == '__main__':
